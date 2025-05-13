9f02110d9b2792ba3cdaf6413adc11c21bfe1089
Ozan Kocatepe <okoc0876@uni.sydney.edu.au>
2025-05-13 20:09:30 +1000

    Finished the code to parse through our data.

--- ParseData.py
+++ ParseData.py
@@ -1,3 +1,4 @@
+import os
 import csv
 from typing import Tuple
 import matplotlib.pyplot as plt
@@ -6,7 +7,7 @@ from scipy import stats
 LOG_FOLDER = "./350mAh_logs"
 OUTPUT_FOLDER = "."
 
-def LinearBatteryUsageFromFile(fileName: str) -> float:
+def ExtractBatteryUsageRateFromFile(fileName: str) -> float:
     """Gets the rate of battery usage from a file.
     
     Assumes the battery usage is linear and fits a
@@ -22,10 +23,12 @@ def LinearBatteryUsageFromFile(fileName: str) -> float:
             This will have units of V/s.
     """
 
-    slope = stats.linregress(timestamps, batteryLevels)
+    timestamps, batteryLevels = ExtractBatteryUsageDataFromFile(fileName)
+
+    slope, intercept, r_value, p_value, std_err = stats.linregress(timestamps, batteryLevels)
     return slope
 
-def ExtractBatteryUsageFromFile(fileName: str) -> Tuple[list[float], list[float]]:
+def ExtractBatteryUsageDataFromFile(fileName: str) -> Tuple[list[float], list[float]]:
     """Extracts the battery usage data from a file.
     
     Parameters:
@@ -56,7 +59,7 @@ def ExtractBatteryUsageFromFile(fileName: str) -> Tuple[list[float], list[float]
        batteryLevels.append(float(line["batteryV"]))
 
     # Shifts the timestamps to start at 0.
-    timestamps = [(time - timestamps[0]) for time in timestamps]
+    timestamps = [(time - timestamps[0]) / 1000.0 for time in timestamps]
 
     return timestamps, batteryLevels
 
@@ -99,9 +102,25 @@ def ExtractHeaderFromFile(fileName: str) -> Tuple[float, float, float, bool]:
     # Stores the lines as an array.
     lines = file.readlines()
 
-    distance = 
+    velocity = lines[5].split(":")
+    velocity = float(velocity[1])
 
-def ExtractBatteryUsageFromFolder(folder: str) -> dict[list[float], float]:
+    horizontalSeparation = lines[6].split(":")
+    horizontalSeparation = float(horizontalSeparation[1])
+
+    verticalSeparation = lines[7].split(":")
+    verticalSeparation = float(verticalSeparation[1])
+
+    heightAboveDefault = lines[8].split(":")
+    heightAboveDefault = float(heightAboveDefault[1])
+    if (heightAboveDefault == 0.0):
+        leading = False
+    else:
+        leading = True
+
+    return velocity, horizontalSeparation, verticalSeparation, leading
+
+def ExtractBatteryUsageFromFolder(folder: str) -> dict[tuple[float, float, float, bool], float]:
     """Extracts the battery usage for each trial from a folder.
 
     Automatically averages all the trials for the same configuration
@@ -113,9 +132,9 @@ def ExtractBatteryUsageFromFolder(folder: str) -> dict[list[float], float]:
             Assumes the only thing in this folder is valid .csv files.
 
     Returns:
-        dict[list[float], float]:
+        dict[list[float, float, float, bool], float]:
             A dictionary where the keys are a list of floats of the form
-            [velocity, horizontalSeparation, verticalSeparation]
+            [velocity, horizontalSeparation, verticalSeparation, leading]
             and the values are the average battery usage for that
             configuration in V/s.
     """
@@ -125,4 +144,29 @@ def ExtractBatteryUsageFromFolder(folder: str) -> dict[list[float], float]:
     # Stores the number of entries that have been averaged so far in each key.
     numEntries = {}
 
-    
+    for file in os.listdir(folder):
+        vel, horiz, vert, leading = ExtractHeaderFromFile(folder + "/" + file)
+        key = (vel, horiz, vert, leading)
+
+        rate = ExtractBatteryUsageRateFromFile(folder + "/" + file)
+
+        # If we've already seen this entry before.
+        if key in output.keys():
+            # Get the number of entries making up the current running average.
+            count = numEntries[key]
+            # Multiply by count, add the new rate, and then take the new average.
+            output[key] = ((output.get(key) * count) + rate) / (count + 1.0)
+            # Update the number of entries in the running average.
+            numEntries[key] += 1
+        else:
+            # Otherwise, just put the value into the dictionary.
+            output[key] = rate
+            numEntries[key] = 1
+
+    return output
+
+rates = ExtractBatteryUsageFromFolder(LOG_FOLDER)
+print("(velocity (m/s), horizontal (m), vertical (m), leading), rate (V/s)")
+for key in rates.keys():
+    print(f"{key}, {rates[key]}")
+print(f"Total number of unique datasets: {len(rates.keys())}")
